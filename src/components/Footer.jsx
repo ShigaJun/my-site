ec880791d41654037ba490ff30ffe563a1c99388
ShigaJun(cTwT) <shigajunctwt@gmail.com>
2024-12-27 01:53:31 +0900

    フッターに「ページ上部へ移動」ボタンを追加

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,14 @@
 export default function Footer() {
     return (
         <footer className="footer">
-            <button>ページトップへ移動</button>
+            <button onClick={returnTop}>ページトップへ移動</button>
         </footer>
     );
-  }
+}
+
+const returnTop = () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+    });
+};
