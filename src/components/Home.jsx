d0179b0bc5be0e9b944775d6d17121703251f1d9
ShigaJun(cTwT) <shigajunctwt@gmail.com>
2025-08-05 22:07:30 +0900

    所属，免許・資格を修正

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,79 +3,91 @@ export default function Home() {
         <main>
             <h2>自己紹介</h2>
             <p>渡邊 陽翔 -Watanabe Haruto-</p>
-            <p>
-                <a href="https://dept.chs.nihon-u.ac.jp/information_science/" target="_blank">
-                日本大学文理学部情報科学科
-                </a>
-            </p>
-            <p>
-                <a href="https://chs.nihon-u.ac.jp/research/rings/" target="_blank">
-                次世代社会研究センター RINGS
-                </a>
-            </p>
+
             <img src="images/my-icon.jpg" alt="渡邊陽翔のプロフィール写真" />
+
+            <h3>所属</h3>
+            <ul>
+                <li>
+                    <p>
+                        <a href="https://dept.chs.nihon-u.ac.jp/information_science/" target="_blank">日本大学文理学部情報科学科</a>
+                    </p>
+                </li>
+                <li>
+                    <p>
+                        <a href="https://vdslab.jp/" target="_blank">尾上研究室(vdslab)</a>
+                    </p>
+                </li>
+                <li>
+                    <p>
+                        <a href="https://chs.nihon-u.ac.jp/research/rings/" target="_blank">次世代社会研究センター RINGS</a>
+                    </p>
+                </li>
+
+            </ul>
+
 
             <h2>経歴</h2>
             <h3>学歴</h3>
             <ul>
-                <li>2023年　日本大学文理学部情報科学科　入学</li>
-                <li>2023年　神奈川県立相模原高等学校　卒業</li>
+                <li>2020年4月　神奈川県立相模原高等学校　入学</li>
+                <li>2023年3月　神奈川県立相模原高等学校　卒業</li>
+                <li>2023年4月　日本大学文理学部情報科学科　入学</li>
             </ul>
 
-            <h2>資格</h2>
+            <h2>免許・資格</h2>
+            <h3>IT系</h3>
             <ul>
-                <li>基本情報技術者試験(2025年6月)</li>
-                <li>TOEIC® Listening & Reading Test IP 715点(2024年12月)</li>
-                <li>実用英語技能検定2級(2021年7月)</li>
-                <li>普通自動車第一種運転免許(2025年5月)</li>
-                <li>全日本空手道連盟　空手道初段(2022年5月)</li>
-                <li>全日本剣道連盟　剣道初段(2018年4月)</li>
+                <li>基本情報技術者試験（2025年6月）</li>
+            </ul>
+            <h3>外国語</h3>
+            <ul>
+                <li>実用英語技能検定2級（2021年7月）</li>
+                <li>TOEIC® Listening & Reading IPテスト　715点（2024年12月）</li>
+            </ul>
+            <h3>その他</h3>
+            <ul>
+                <li>全日本剣道連盟　剣道初段（2018年4月）</li>
+                <li>全日本空手道連盟　空手道初段（2022年5月）</li>
+                <li>普通自動車第一種運転免許（2025年5月）</li>
             </ul>
 
             <h2>連絡先</h2>
             <h3>E-mail</h3>
             <p>
                 <a href="mailto:watanabe.haruto.4869@gmail.com">
-                watanabe.haruto.4869@gmail.com
+                    watanabe.haruto.4869@gmail.com
                 </a>
             </p>
 
             <h2>リンク集</h2>
             <h3>𝕏</h3>
             <p>
-                <a href="https://x.com/wata_haru_4869" target="_blank">
-                @wata_haru_4869
-                </a>
+                <a href="https://x.com/wata_haru_4869" target="_blank">@wata_haru_4869</a>
             </p>
             <h3>GitHub</h3>
             <p>
-                <a href="https://github.com/ShigaJun" target="_blank">
-                https://github.com/ShigaJun
-                </a>
+                <a href="https://github.com/ShigaJun" target="_blank">https://github.com/ShigaJun</a>
             </p>
-            <h3>note</h3>
+            <h3>AtCoder</h3>
             <p>
-                <a href="https://note.com/wata_haru_4869/" target="_blank">
-                https://note.com/wata_haru_4869/
-                </a>
-            </p>
-            <h3>Qiita</h3>
-            <p>
-                <a href="https://qiita.com/wata_haru_4869" target="_blank">
-                https://qiita.com/wata_haru_4869
-                </a>
-            </p>
-            <h3>YouTube</h3>
-            <p>
-                <a href="https://www.youtube.com/@JunShiga_cTwT" target="_blank">
-                https://www.youtube.com/@JunShiga_cTwT
-                </a>
+                <a href="https://atcoder.jp/users/WatanabeHaruto" target="_blank">https://atcoder.jp/users/WatanabeHaruto</a>
             </p>
             <h3>unityroom</h3>
             <p>
-                <a href="https://unityroom.com/users/5qblj942rh1vw3ndk6fe" target="_blank">
-                https://unityroom.com/users/5qblj942rh1vw3ndk6fe
-                </a>
+                <a href="https://unityroom.com/users/5qblj942rh1vw3ndk6fe" target="_blank">https://unityroom.com/users/5qblj942rh1vw3ndk6fe</a>
+            </p>
+            <h3>Qiita</h3>
+            <p>
+                <a href="https://qiita.com/wata_haru_4869" target="_blank">https://qiita.com/wata_haru_4869</a>
+            </p>
+            <h3>note</h3>
+            <p>
+                <a href="https://note.com/wata_haru_4869/" target="_blank">https://note.com/wata_haru_4869/</a>
+            </p>
+            <h3>YouTube</h3>
+            <p>
+                <a href="https://www.youtube.com/@JunShiga_cTwT" target="_blank">https://www.youtube.com/@JunShiga_cTwT</a>
             </p>
         </main>
     );
