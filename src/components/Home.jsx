33c3e2f949f39587dd5f06b5329bfe755714ae5e
ShigaJun(cTwT) <shigajunctwt@gmail.com>
2025-08-05 23:41:09 +0900

    リンク集を「関連リンク」としてページ遷移するように変更

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -59,36 +59,6 @@ export default function Home() {
                     watanabe.haruto.4869@gmail.com
                 </a>
             </p>
-
-            <h2>リンク集</h2>
-            <h3>𝕏</h3>
-            <p>
-                <a href="https://x.com/wata_haru_4869" target="_blank">@wata_haru_4869</a>
-            </p>
-            <h3>GitHub</h3>
-            <p>
-                <a href="https://github.com/ShigaJun" target="_blank">https://github.com/ShigaJun</a>
-            </p>
-            <h3>AtCoder</h3>
-            <p>
-                <a href="https://atcoder.jp/users/WatanabeHaruto" target="_blank">https://atcoder.jp/users/WatanabeHaruto</a>
-            </p>
-            <h3>unityroom</h3>
-            <p>
-                <a href="https://unityroom.com/users/5qblj942rh1vw3ndk6fe" target="_blank">https://unityroom.com/users/5qblj942rh1vw3ndk6fe</a>
-            </p>
-            <h3>Qiita</h3>
-            <p>
-                <a href="https://qiita.com/wata_haru_4869" target="_blank">https://qiita.com/wata_haru_4869</a>
-            </p>
-            <h3>note</h3>
-            <p>
-                <a href="https://note.com/wata_haru_4869/" target="_blank">https://note.com/wata_haru_4869/</a>
-            </p>
-            <h3>YouTube</h3>
-            <p>
-                <a href="https://www.youtube.com/@JunShiga_cTwT" target="_blank">https://www.youtube.com/@JunShiga_cTwT</a>
-            </p>
         </main>
     );
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Articles from "./Articles";
+import Links from "./Links";
 
 export default function Nav() {
     return (
@@ -10,12 +11,14 @@ export default function Nav() {
                     <ul>
                         <li><Link to="/">ホーム</Link></li>
                         <li><Link to="/articles">投稿記事</Link></li>
+                        <li><Link to="/links">関連リンク</Link></li>
                     </ul>
                 </nav>
 
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="articles" element={<Articles />} />
+                    <Route path="links" element={<Links />} />
                 </Routes>
             </div>
         </BrowserRouter>
