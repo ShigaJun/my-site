143389e8d11eb4d651cc437e40acafa122e45b61
ShigaJun(cTwT) <shigajunctwt@gmail.com>
2025-08-05 22:06:50 +0900

    フッターにコピーライトを追加

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,7 @@ export default function Footer() {
     return (
         <footer className="footer">
             <button onClick={returnTop}>ページトップへ移動</button>
+            <p>&copy; 2025 Watanabe Haruto</p>
         </footer>
     );
 }
