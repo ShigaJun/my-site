5dfd514d19d3fb6b22ee752ced0bc662c08fe697
Watanabe Haruto <watanabe.haruto.4869@gmail.com>
2026-01-26 01:28:14 +0900

    index.css の適用，styel.css の削除

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
@@ -8,7 +9,7 @@ export default defineConfig({
       "/.netlify/functions": "http://localhost:9999",
     },
   },
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
