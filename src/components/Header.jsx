d96258ffd15cc08a0f0ab899b4f24585944e056e
Watanabe Haruto <watanabe.haruto.4869@gmail.com>
2026-05-10 01:48:13 +0900

    ヘッダーを修正

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,22 @@
+import { Link } from "react-router-dom";
+
 import Nav from "./Nav";
 
 export default function Header() {
-    return (
-        <header className="header">
-            <h1 className="title">Watanabe Haruto</h1>
-            <Nav />
-        </header>
-    );
+  return (
+    <header className="flex items-center justify-between h-20 px-6 border-b">
+      <Link to="/">
+        <span className="text-xl font-semibold">
+          <img
+            src="images/logo.png"
+            alt="Logo"
+            className="inline-block size-8 mr-2"
+          />
+          Watanabe Haruto
+        </span>
+      </Link>
+
+      <Nav />
+    </header>
+  );
 }
